e32c87062bc2077c45329c805e7cb58cca19b055
Matheus Andre <matheusandr2@gmail.com>
2024-05-31 22:30:11 -0300

    terminando com o challenge 01

---
+++ challenge-01/main.js
@@ -0,0 +1,22 @@
+let myvar;
+
+myvar = 10;
+
+let soma = 15 + 5;
+
+soma += 1;
+soma *= 3;
+console.log(soma)
+
+let souninja = true;
+let comida = ["arroz", "feijão", "ovo"];
+console.log(comida[1]);
+
+console.log(soma === myvar);
+console.log(myvar <= soma);
+
+function divisao(num1,num2){
+    return console.log(num1 / num2);
+}
+
+divisao(10,2);
