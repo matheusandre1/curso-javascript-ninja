62357885c892586d978a12db529194acf7fc6cb3
Matheus Andre <matheusandr2@gmail.com>
2024-06-02 02:49:22 -0300

    terminando outra parte do desafio

--- challenge-03/main.js
+++ challenge-03/main.js
@@ -2,12 +2,12 @@
 let myobj = {};
 
 let pessoa = {
-    nome: String,
-    sobrenome: String,
-    sexo: String,
+    nome: "Jose",
+    sobrenome: "Andre",
+    sexo: "Masculino",
     idade: 10,
-    altura:Number,
-    peso: Number,
+    altura: 1.94,
+    peso: 100,
     andando: false,
     caminhouQuantosMetros: 0,
     fazerAniversario: function () {
@@ -15,7 +15,7 @@ let pessoa = {
     },
     andar: function (valor) {
         this.caminhouQuantosMetros += valor;
-        this.andando = true;
+        return this.andando = true;
     },
     parar: function () {
         return this.andando = false;
@@ -29,14 +29,28 @@ let pessoa = {
     mostrarPeso: function () {
         return `Eu peso ${this.peso} anos!`;
     },
-    mostrarAltura: function(){
+    mostrarAltura: function () {
         return `Minha Altura é ${this.altura}m`;
     }
 };
 
+console.log(`Qual é a idade da pessoa: ${pessoa.idade}`);
+console.log(`Qual é a peso da pessoa: ${pessoa.peso}`);
+console.log(`Qual é a altura da pessoa: ${pessoa.altura}`);
+
+pessoa.fazerAniversario();
+pessoa.fazerAniversario();
+pessoa.fazerAniversario();
+console.log(`Aniversário da pessoa daqui a 3 anos: ${pessoa.fazerAniversario()}`);
+console.log(`A idade atual da Pessoa é ${pessoa.idade}`); //14
 pessoa.andar(50);
-console.log(pessoa.caminhouQuantosMetros)
-console.log(pessoa.andando)
-console.log(pessoa.parar())
+pessoa.andar(100);
+console.log(pessoa.andar(20));
+pessoa.parar();
+console.log(pessoa.parar()); //false
+console.log(pessoa.caminhouQuantosMetros); //170
+
+
+
 
 
